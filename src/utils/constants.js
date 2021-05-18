1fc46030fd7de084001ed6ac9a86b4af45ca2e63
AmandaKhol <amanda.camsua@gmail.com>
2021-05-18 16:28:37 +0200

    chore: add constant values

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,6 +2,6 @@ const heightCanvas = 100;
 const widthCanvas = 200;
 
 const heightHue = 30;
-const widthHue = 200;
+const widthHue = 300;
 
 export { heightCanvas, widthCanvas, heightHue, widthHue };
