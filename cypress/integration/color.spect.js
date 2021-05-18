e80ee196c756ddf9d9778c1fddd80ccf9f69fde6
AmandaKhol <amanda.camsua@gmail.com>
2021-05-18 14:40:32 +0200

    test: adding test with cypress

---
+++ cypress/integration/color.spect.js
@@ -0,0 +1,49 @@
+describe('renders the home page with default values', () => {
+  it('renders correctly', () => {
+    cy.visit('/');
+    cy.get('main').should('exist');
+  });
+  it('starts with red color', () => {
+    cy.visit('index.html');
+    cy.get('.color__input--input').should('have.value', '#ff0000');
+    cy.get('.color__picker--selected').should(
+      'have.css',
+      'background-color',
+      'rgb(255, 0, 0)'
+    );
+  });
+});
+
+describe('change the values when color change', () => {
+  it('changes input green color value', () => {
+    cy.visit('index.html');
+    cy.get('.color__input--input').clear().type('00ff00').trigger('change');
+    cy.get('.color__picker--selected').should(
+      'have.css',
+      'background-color',
+      'rgb(0, 255, 0)'
+    );
+  });
+  it('changes with hue canvas click', () => {
+    cy.visit('index.html');
+    cy.get('.hue').click(200, 5);
+    cy.get('.color__picker--selected').should(
+      'have.css',
+      'background-color',
+      'rgb(3, 0, 255)'
+    );
+    cy.get('.color__input--input').should('have.value', '#0300ff');
+    cy.get('.picker').should('have.css', 'top', '-10px');
+    cy.get('.picker').should('have.css', 'left', '290px');
+  });
+  it('changes with gradient canvas click', () => {
+    cy.visit('index.html');
+    cy.get('.color__panel--canvas').click(200, 5);
+    cy.get('.color__picker--selected').should(
+      'have.css',
+      'background-color',
+      'rgb(241, 80, 80)'
+    );
+    cy.get('.color__input--input').should('have.value', '#f15050');
+  });
+});
